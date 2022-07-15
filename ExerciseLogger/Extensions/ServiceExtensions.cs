1c0eae004ea3062bd8c80595001b80cde575c448
Walter4B <walter.bali.markovi@gmail.com>
2022-07-15 17:03:21 +0200

    Add project files.

---
+++ ExerciseLogger/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace ExerciseLogger.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //Cors configuration give or restrict access to application from different domains
+        //For production restrictions are necessary
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        //IIS configuration 
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
