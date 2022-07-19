5a5ff71cf258c783058e9559948d099e7afe54de
Walter4B <walter.bali.markovi@gmail.com>
2022-07-19 16:20:44 +0200

    Added: -Repository context -Repo context factory

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Gym>? Gyms { get; set; }
+        public DbSet<Exercise>? Exercises { get; set; }
+    }
+}
