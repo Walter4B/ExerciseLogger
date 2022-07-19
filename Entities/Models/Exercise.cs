8a470ed3f1b28b9964c47fc71900b11bd8ac543b
Walter4B <walter.bali.markovi@gmail.com>
2022-07-19 16:01:08 +0200

    Added Models for exercise sessions

---
+++ Entities/Models/Exercise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Exercise
+    {
+        [Column("ExerciseId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Exercise type is required.")]
+        [MaxLength(40, ErrorMessage = "Maximum lenght for the exercise type is 40 characters.")]
+        public string? Type { get; set; }
+
+        [Required(ErrorMessage = "Exercise start time is required")]
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        [MaxLength(300, ErrorMessage = "Maximum lenght for the comment is 300 characters")]
+        public string? Comments { get; set; }
+
+        [ForeignKey(nameof(Gym))]
+        public Guid GymId { get; set; }
+        public Gym? Gym { get; set; }
+    }
+}
---
+++ Entities/Models/Gym.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Gym
+    {
+        [Column("GymId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Gym name is required.")]
+        [MaxLength(40, ErrorMessage = "Maximum lenght for the name is 40 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenght for the address is 50 characters")]
+        public string? Address { get; set; }
+
+        public ICollection<Exercise>? Exercises { get; set; }
+    }
+}
