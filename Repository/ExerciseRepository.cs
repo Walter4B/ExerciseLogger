0a2764c8701498bb8e67cd49fbfdc3a685937ffc
Walter4B <walter.bali.markovi@gmail.com>
2022-07-30 16:33:04 +0200

    Added repository user classes

---
+++ Repository/ExerciseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Contracts;
+
+namespace Repository
+{
+    public class ExerciseRepository : RepositoryBase<Exercise>, IExerciseRepository
+    {
+        public ExerciseRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+    }
+}
---
+++ Repository/GymRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Contracts;
+
+namespace Repository
+{
+    public class GymRepository : RepositoryBase<Gym>, IGymRepository
+    {
+        public GymRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+    }
+}
