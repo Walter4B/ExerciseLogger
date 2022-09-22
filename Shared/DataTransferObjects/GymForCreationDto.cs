994fc2ab00fb818655cf9a46f743334a5e50b956
Walter4B <walter.bali.markovi@gmail.com>
2022-09-23 00:36:25 +0200

    Added: -adding gym with existing exercises

--- Shared/DataTransferObjects/GymForCreationDto.cs
+++ Shared/DataTransferObjects/GymForCreationDto.cs
@@ -6,5 +6,5 @@ using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects
 {
-    public record GymForCreationDto(string Name, string Address);
+    public record GymForCreationDto(string Name, string Address, IEnumerable<ExerciseForCreationDto> Exercises);
 }
